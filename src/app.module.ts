84f85268d753b5c60b09e2c18a7710ccc7e9d692
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2024-01-27 03:24:07 +0900

    モジュールを定義

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { OrderBookClient } from './order-book/websocket.client';
+import { OrderBookService } from './order-book/order-book.service';
+import { DataSourceModule } from './data-source.module';
+import { OrderBookDiff } from './entities/order-book-diff.entity';
+import { OrderBookSnapshot } from './entities/order-book-snapshot.entity';
+
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { OrderBookSnapshot } from './entities/order-book-snapshot.entity'; //まだ存在しないが仮
-// import { OrderBookUpdate } from './entities/order-book-update.entity'; //まだ存在しないが仮
-import { MarketsService } from './markets/markets.service';
+
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres', // データベースの種類
-      host: 'localhost', // データベースのホスト
-      port: 5432, // データベースのポート
-      username: 'postgres', // ユーザー名
-      password: '1373', // パスワード
-      database: 'orderBookDB', // データベース名
-      // entities: [OrderBookSnapshot, OrderBookUpdate],
-      synchronize: true, // 開発中は true に設定すると便利です（実稼働環境では false 推奨）
-    }),
+  imports: [DataSourceModule,
+    TypeOrmModule.forFeature([OrderBookDiff]),
+    TypeOrmModule.forFeature([OrderBookSnapshot]),
   ],
-  controllers: [AppController],
-  providers: [AppService, MarketsService],
+  controllers: [],
+  providers: [OrderBookClient, OrderBookService],
 
 })
 export class AppModule { }
