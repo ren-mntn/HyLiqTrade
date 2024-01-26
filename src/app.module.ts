f707e5cd3a0c959aa854c2990e6c338347945b06
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2024-01-26 14:49:57 +0900

    TypeORMのセットアップ

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderBookSnapshot } from './entities/order-book-snapshot.entity'; //まだ存在しないが仮
+import { OrderBookUpdate } from './entities/order-book-update.entity'; //まだ存在しないが仮
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', // データベースの種類
+      host: 'localhost', // データベースのホスト
+      port: 5432, // データベースのポート
+      username: 'postgres', // ユーザー名
+      password: '1373', // パスワード
+      database: 'orderBookDB', // データベース名
+      entities: [OrderBookSnapshot, OrderBookUpdate],
+      synchronize: true, // 開発中は true に設定すると便利です（実稼働環境では false 推奨）
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
+
 })
-export class AppModule {}
+export class AppModule { }
